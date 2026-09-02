63cdd4698be67af753bb608a70b3a70f4e6a865f
jokyuhyeok <khjo3554@gmail.com>
2026-09-02 15:58:23 +0900

    feat: 총알 발사 쿨타임 적용

---
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    // 목표 : space bar를 누를때마다 총알을 생성해서 발사하고 싶다.
+    // 필요속성 : 총알 프리팹
+    public GameObject BulletPrefab;
+    
+    // 필요속성 2: 생성위치(총구)
+    public Transform FirePoint_Left;
+    public Transform FirePoint_Right;
+    
+    // 쿨타임 변수
+    public float CoolDown_time = 5.0f;
+    public float Current_time = 0.0f;
+    
+    private void Update()
+    {
+        // [정리과제 6] 총알 발사에 있어 쿨타임 적용
+        if (Current_time > 0.0f)
+        {
+            Current_time -= Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && Current_time <= 0.0f)
+        {
+            Fire();
+            Current_time = CoolDown_time;
+        }
+    }
+
+    private void Fire()
+    {
+        // 1. 스페이스바를 누르면
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // 2. 총알 프리팹을 생성한다.
+            // Instantiate는 프리팹을 복사해서 게임 오브젝트를 만들고 씬에 넣어주는 기능
+            GameObject bullet1 = Instantiate(BulletPrefab);
+            bullet1.transform.position = FirePoint_Left.position; // 생성한 총알의 위치를 총구의 위치로
+            
+            GameObject bullet2 = Instantiate(BulletPrefab);
+            bullet2.transform.position = FirePoint_Right.position;
+            
+
+        }
+    }
+}
