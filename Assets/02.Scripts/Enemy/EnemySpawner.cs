fa1b847ee0e037bcaf63fd5a7e7c691fd75f61d8
jokyuhyeok <khjo3554@gmail.com>
2026-09-04 12:45:54 +0900

    feat: 적 자동 생성해주는 적 스포너

---
+++ Assets/02.Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+// 역할 : 일정 시간마다 적을 생성해주고 싶다.
+public class EnemySpawner : MonoBehaviour
+{
+    // 필요 속성
+    // - 타이머
+    [Header("스폰 간격")][SerializeField] private float _spawnInternal = 3f;
+    private float _timer;
+
+    // - 생성할 프리팹
+    [Header("스폰할 적 프리팹")][SerializeField] private Enemy _enemyPrefab;
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer >= _spawnInternal)
+        {
+            _timer = 0f;
+
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        Enemy enemy = Instantiate(_enemyPrefab);
+        enemy.transform.position = transform.position;
+    }
+}
