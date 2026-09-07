40350884c5d490f88a689575bc856da1c52cecbc
jokyuhyeok <khjo3554@gmail.com>
2026-09-07 12:42:28 +0900

    feat: 2초 대기 후 플레이어에게 이동하는 아이템 기능 구현

--- Assets/02.Scripts/Item/Item.cs
+++ Assets/02.Scripts/Item/Item.cs
@@ -1,9 +1,34 @@
+using System;
 using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    public ItemType Type;
-    public float Value;
+    [SerializeField] private ItemType _type;
+    [SerializeField] private float _value;
+
+    private const float WaitTime = 2.0f;
+    private float _waitTimer = 0f;
+    private const float MoveSpeed = 5f;
+
+    private void Update()
+    {
+        _waitTimer += Time.deltaTime;
+        if (_waitTimer >= WaitTime)
+        {
+            FollowPlayer();
+        }
+    }
+
+    private void FollowPlayer()
+    {
+        Player player = GameObject.FindWithTag("Player").GetComponent<Player>();
+
+        if (player == null)
+        {
+            Debug.LogWarning("플레이어를 찾을 수 없습니다.");
+            return;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -16,11 +41,11 @@ public class Item : MonoBehaviour
             return;
         }
 
-        switch (Type)
+        switch (_type)
         {
             case ItemType.Heal:
                 {
-                    player.Heal((int)(Value));
+                    player.Heal((int)(_value));
                     break;
                 }
 
@@ -29,13 +54,13 @@ public class Item : MonoBehaviour
                     // 캡슐화 : 
                     // + 데이터 은닉(Speed 속성 private 처리) 
                     // + 행위를 통한 상태 변경 (SpeedUp 호출)
-                    player.GetComponent<PlayerMove>().SpeedUp(Value);
+                    player.GetComponent<PlayerMove>().SpeedUp(_value);
                     break;
                 }
 
             case ItemType.FireRateUp:
                 {
-                    player.GetComponent<PlayerFire>().FireRateUp(Value);
+                    player.GetComponent<PlayerFire>().FireRateUp(_value);
                     break;
                 }
         }
