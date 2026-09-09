37f018b9fba87e9df114ecf1e04f5bf5e59e067a
jokyuhyeok <khjo3554@gmail.com>
2026-09-09 12:38:01 +0900

    chore: 적 프리팹 수치 수정/ 1차 중간영상 녹화

--- Assets/02.Scripts/Player/Player.cs
+++ Assets/02.Scripts/Player/Player.cs
@@ -6,29 +6,26 @@ public class Player : MonoBehaviour
     // - 데이터 은닉
     // - 메서드를 통한 상태 변경
     [SerializeField] private int _health = 100;
-
     [SerializeField] private GameObject _playerdeathEffectPrefab;
     [SerializeField] private GameObject _playerHealEffectPrefab;
 
     public int Health => _health; // 람다식 문법을 활용한 읽기 전용 프로퍼티
-    //{
-    //    get { return _health; }
-    //}
 
+    //{
+    // get { return _health; }
+    //
     // 잘 설계된 클래스는
     // - 필드 (인스턴스 변수)
     // - 필드에 잘못된 값이 할당되지 않게 막고, 정상적으로 동작하는 메서드
-
-    // getter/setter  : 특정 데이터를 get/set 해주는 메서드
+    // getter/setter : 특정 데이터를 get/set 해주는 메서드
     //public void SetHealth(int value)
     //
-    //    _health = value;
+    // _health = value;
     //}
     //public int GetHealth()
     //{
-    //    return _health;
+    // return _health;
     //}
-
     public void TakeDamage(int damage)
     {
         if (damage < 0)
@@ -38,6 +35,7 @@ public class Player : MonoBehaviour
         }
 
         _health -= damage;
+
         if (_health <= 0)
         {
             SpawnPlayerDeathEffect();
