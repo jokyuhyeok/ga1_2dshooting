ff43d07bd0ff3ad5850e6915e3d0f93268a4a706
jokyuhyeok <khjo3554@gmail.com>
2026-09-10 13:50:55 +0900

    feat: 점수 관리자에 싱글톤 적용
    static 키워드를 이용한 정적관리
    Instance를 사용

--- Assets/02.Scripts/Enemy/Enemy.cs
+++ Assets/02.Scripts/Enemy/Enemy.cs
@@ -87,8 +87,7 @@ public abstract class Enemy : MonoBehaviour
             SpawnDeathEffect();
             SpawnItem();
 
-            ScoreManager scoreManager = FindAnyObjectByType<ScoreManager>();
-            scoreManager.AddScore(100);
+            ScoreManager.Instance.AddScore(100);
 
             Destroy(gameObject);
         }
